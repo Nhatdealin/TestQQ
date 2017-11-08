9c381e5b5a80d6e4130ca4031ae4713f7cff9fce
Nhatdealin <32137330+Nhatdealin@users.noreply.github.com>
2017-11-08 20:48:09 +0700

    Add files via upload

---
+++ EducationActivity.java
@@ -0,0 +1,17 @@
+package com.example.user.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by USER on 11/4/2017.
+ */
+
+public class EducationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.education_main);
+    }
+}
